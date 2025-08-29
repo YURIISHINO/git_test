f72027266647d4930b3c063f5c2d85318516d6da
YURI <D24004@hama-med.ac.jp>
2025-08-29 14:19:58 +0900

    name

--- git_test.R
+++ git_test.R
@@ -1,7 +1,11 @@
 install.packages("readxl")
 library(readxl)
-
-death <- read_excel("death.xlsx")
+death <- read_excel("E:/R/death.xlsx")
 death
 
 print(death)
+View(death)
+
+git remote add origin https://github.com/YURIISHINO/git_test.git
+
+View(death)
